5afa13321f7918121321b098572e5efbd8417e54
christianalim <christianalim1@gmail.com>
2019-09-30 17:12:07 -0500

    nvm

--- app/models/hub.rb
+++ app/models/hub.rb
@@ -2,7 +2,7 @@ class Hub < ApplicationRecord
     has_many :reviews, dependent: :destroy
     has_many :users, through: :reviews
     has_many :images
-    
+
     def get_rating
         reviews = self.reviews
         ratings = []
--- app/serializers/hub_serializer.rb
+++ app/serializers/hub_serializer.rb
@@ -1,3 +1,4 @@
 class HubSerializer < ActiveModel::Serializer
   attributes :id, :name, :latitude, :longitude, :address, :rating, :description, :wifi, :restrooms, :noise, :reviews
+  
 end
